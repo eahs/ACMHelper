e2698a2f8ddeeb82471db81c57a94fb1c377d6ee
Michael Tanczos <mike@logic-gate.com>
2019-03-22 10:49:26 -0400

    Added DotTwist

--- Java/src/ACMTest/DotTwist.java
+++ Java/src/ACMTest/DotTwist.java
@@ -7,7 +7,7 @@ public class DotTwist {
     public static void main (String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.print("Dice: ");
+        System.out.print("Enter three coordinates: ");
         String input = scan.nextLine();
 
         int[] a = ACM.toIntegerArray(input);
@@ -15,6 +15,8 @@ public class DotTwist {
 
         Arrays.sort(b);
 
+        int soln = a[0]*b[0] + a[1]*b[1] + a[2]*b[2];
 
+        System.out.println("Dot twist: " + soln);
     }
 }
