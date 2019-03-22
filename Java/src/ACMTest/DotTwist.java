6d6570bde3c9d307a7529949d0892b8d6ea6b348
Michael Tanczos <mike@logic-gate.com>
2019-03-22 10:20:39 -0400

    Added toIntegerArray

---
+++ Java/src/ACMTest/DotTwist.java
@@ -0,0 +1,20 @@
+import com.sun.javafx.geom.Vec3d;
+
+import java.util.*;
+
+public class DotTwist {
+
+    public static void main (String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Dice: ");
+        String input = scan.nextLine();
+
+        int[] a = ACM.toIntegerArray(input);
+        int[] b = a.clone();  // Make a copy of a
+
+        Arrays.sort(b);
+
+
+    }
+}
